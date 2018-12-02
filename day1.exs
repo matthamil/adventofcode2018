fb1fa63973de41afc6a3883272b004f1207ce198
Matt Hamil <j.matthew.hamil@gmail.com>
2018-12-02 14:02:16 -0600

    :tada: initial commit

---
+++ day1.exs
@@ -0,0 +1,44 @@
+defmodule Day1 do
+  def final_frequency(file_stream) do
+    file_stream
+    |> Stream.map(&parse_line_to_integer/1)
+    |> Enum.sum()
+  end
+
+  defp parse_line_to_integer(line) do
+    {integer, _leftover} = Integer.parse(line)
+    integer
+  end
+end
+
+case System.argv() do
+  ["--test"] ->
+    ExUnit.start()
+
+    defmodule Day1Test do
+      use ExUnit.Case
+
+      import Day1
+
+      test "final_frequency" do
+        {:ok, io} =
+          StringIO.open("""
+          +1
+          +1
+          +1
+          """)
+
+        assert final_frequency(IO.stream(io, :line)) == 3
+      end
+    end
+
+  [input_file] ->
+    input_file
+    |> File.stream!([], :line)
+    |> Day1.final_frequency()
+    |> IO.puts()
+
+  _ ->
+    IO.puts(:stderr, "Expected --test flag or an input file")
+    System.halt(1)
+end
